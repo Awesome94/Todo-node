90025b66b7650eddfb3a52c23562039ad69d6949
awesome94 <david.kironde@andela.com>
2019-09-23 20:05:36 +0300

    add post request

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser'
 
 const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/api/V1/todos', (req, res) =>{
     res.status(200).send({
@@ -11,8 +17,26 @@ app.get('/api/V1/todos', (req, res) =>{
     })
 });
 
-const PORT = 5000;
+app.post('/api/v1/todos', (req, res)=>{
+    if(!req.body.title){
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+    const todo = {
+        id: db.length+1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    db.push(todo);
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
+    })
+})
 
-app.listen(PORT, () =>{
-    console.log(`server running on port ${PORT}`)
+app.listen(PORT, ()=>{
+    console.log(`App is running  on ${PORT}`)
 });
