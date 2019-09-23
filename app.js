e50082c2f8d405e1a7da9a0da81b6aa2977845e4
awesome94 <david.kironde@andela.com>
2019-09-23 21:15:34 +0300

    add get for one todo item

--- app.js
+++ app.js
@@ -37,6 +37,23 @@ app.post('/api/v1/todos', (req, res)=>{
     })
 })
 
+app.get('api/v1/todos/:id', (req, res)=>{
+    const id = parseInt(req.params.id, 10);
+    db.map((todo)=>{
+        if (todo.id === id) {
+            return res.status(200).send({
+                success: 'true',
+                message:'todo retrived successfully',
+                todo,
+            });
+        }
+    });
+    return res.status(404).send({
+        success: 'false',
+        message: 'todo does not exist',
+    });
+});
+
 app.listen(PORT, ()=>{
     console.log(`App is running  on ${PORT}`)
 });
