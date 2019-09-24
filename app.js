64fac966a9026180876883d4b0a1d6da0a1c29ae
awesome94 <david.kironde@andela.com>
2019-09-24 16:07:57 +0300

    add update function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './db/db';
 import bodyParser from 'body-parser'
+import { toUnicode } from 'punycode';
 
 const app = express();
 const PORT = 5000;
@@ -70,6 +71,47 @@ app.delete('api/v1/todos/:id', (req, res)=>{
         message: 'todo not found',
     });
 });
+
+app.put('.api/v1/todos/:id', (req, res)=>{
+    const id = parseInt(req.params.id, 10)
+    let todoFound;
+    let itemIndex;
+    db.map((todo, index)=>{
+        if(todo.id === id){
+            todoFound = todo
+            itemIndex = index
+        }
+    });
+    if(!todoFound){
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo not found'
+        });
+    }
+    if(!req.body.title){
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        })
+    }else if(!req.body.description){
+        return res.status(400).send({
+            success: 'false',
+            message: 'description can not be blank'
+        })
+    }
+    const updatedTodo = {
+        id: todoFound.id,
+        title: req.body.title || todoFound.title,
+        description: req.body.description || todoFound.description,
+    };
+    db.splice(itemIndex, 1, updatedTodo);
+
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        updatedTodo,
+    })
+})
 app.listen(PORT, ()=>{
     console.log(`App is running  on ${PORT}`)
 });
