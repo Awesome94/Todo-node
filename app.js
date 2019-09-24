ec92b136b52bae507d7dcbd79285fc682e71fa22
awesome94 <david.kironde@andela.com>
2019-09-24 15:07:04 +0300

    remove todo item from list

--- app.js
+++ app.js
@@ -58,10 +58,18 @@ app.delete('api/v1/todos/:id', (req, res)=>{
     const id = parseInt(req.params.id, 10);
     db.map((todo, index)=>{
         if(todo.id===id){
-            todo.pop(todo)
+            db.splice(index, 1);
+            return res.status(200).send({
+                success: 'true',
+                message: 'Todo deleted successfully',
+            });
         }
     })
-})
+    return res.status(404).send({
+        success: 'false',
+        message: 'todo not found',
+    });
+});
 app.listen(PORT, ()=>{
     console.log(`App is running  on ${PORT}`)
 });
